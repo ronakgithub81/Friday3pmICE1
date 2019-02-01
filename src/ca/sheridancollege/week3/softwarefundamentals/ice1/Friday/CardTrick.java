0a35606a1d3f22f8d6d1b158d751dbd408705d10
zhfxm123 <47042343+zhfxm123@users.noreply.github.com>
2019-02-02 07:17:02 +0900

    Printing out Value and Suit for the created object
    Printing out Value and Suit for the created object

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,6 +9,10 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card limCard = new Card();
+        limCard.setValue(11);
+        limCard.setSuit(Spades);
+        System.out.println(limCard.getValue());
+        System.out.println(limCard.getSuit());
     }
     
 }
