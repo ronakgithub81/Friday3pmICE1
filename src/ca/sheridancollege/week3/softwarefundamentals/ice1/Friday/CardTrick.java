e614fcebff99142787f216db4caadb46a0e48438
uddipamin <aminu@sheridancollege.ca>
2019-02-01 16:45:53 -0500

    Uddip Amin's ICE1

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -4,11 +4,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 /**
  *
  * @author Ronak
+ * Uddip Amin
+ * 991518202
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card card = new Card();
     }
     
 }
