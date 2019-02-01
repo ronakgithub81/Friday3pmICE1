2cc39c456ae63ce1822ac310347bb14fa48d3c84
uddipamin <aminu@sheridancollege.ca>
2019-02-01 17:15:43 -0500

    Uddip Amin's ICE1 Second commit: Added second object of Card

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -11,10 +11,13 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card card = new Card();
+       Card card2 = new Card();
+       
        card.setSuit("Hearts");
        card.setValue(3);
        
-       System.out.println(getSuit() + " " + getValue());
+       
+       System.out.println(card.getSuit() + " " + card.getValue());
     }
     
 }
