53e74f27ed1a5d3328398ae209830d713f4709f9
Javier <44002569+Javibleu@users.noreply.github.com>
2019-02-01 17:21:37 -0500

    Update CardTrick.java
    Creates Both Objects and ad some comments.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,12 +8,11 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-       Card card1 = new Card();
-       Card card2 = new Card();
-       
-       
+       Card card1 = new Card(); //creates the first Object
+       Card card2 = new Card(); //creates the second Object
+                    
         System.out.printf("Suit is: %s, and Value is: %s" ,card1.getSuit(), card1.getValue());
-        System.out.printf("Suit is: %s, and Value is: %s" ,card2.getSuit(), card2.getValue());       
+        System.out.printf("Suit is: %s, and Value is: %s" ,card2.getSuit(), card2.getValue());        
     }
     
     
