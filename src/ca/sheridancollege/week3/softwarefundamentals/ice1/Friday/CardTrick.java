1a509df7208cc6e698a87834b28c0dde619d8b07
tahires <47042342+tahires@users.noreply.github.com>
2019-02-01 17:14:50 -0500

    added set and get for suit and value
    added set and get for suit and value

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,6 +9,10 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card myCard = new Card();
+        myCard.setSuit("Diamond");
+        myCard.setValue(2);
+        System.out.printLn(myCard.getSuit());
+        System.out.printLn(myCard.getValue());
     }
     
 }
