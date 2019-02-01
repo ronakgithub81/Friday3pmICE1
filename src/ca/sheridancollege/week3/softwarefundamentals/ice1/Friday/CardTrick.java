f013202aa728483ee617a7bc7ceefc4a9c4c3ab3
LukaNecajev <necajevl@sheridancollege.ca>
2019-02-01 16:45:45 -0500

    Luka's Assignment

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -4,11 +4,14 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 /**
  *
  * @author Ronak
+ * Luka Necajev
+ * 991475972
+ * 
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card c1=new Card();
     }
     
 }
