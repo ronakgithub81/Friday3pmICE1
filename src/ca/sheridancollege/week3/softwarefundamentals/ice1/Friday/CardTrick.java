5f281fe6488a6638db31acb68f4f332d369621d8
Javier <nievasj@sheridancollege.ca>
2019-02-01 16:48:15 -0500

    Commit Javier

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -2,13 +2,13 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
- *
- * @author Ronak
+ * Student Number 991534369
+ * @author Javier Nievas
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card card1 = new Card();
     }
     
 }
