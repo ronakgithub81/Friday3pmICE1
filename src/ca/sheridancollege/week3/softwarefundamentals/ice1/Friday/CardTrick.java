733e030cbfb6db72bb7d6ad48cd3d34bdb6cc83e
Essa OG <Essa OG@DESKTOP-P8U7JG2>
2019-02-01 17:10:20 -0500

    how are you boi

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,7 +3,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Ronak @modifier Essa Tahir 991524953
  */
 public class CardTrick {
     public static void main(String[] args)
