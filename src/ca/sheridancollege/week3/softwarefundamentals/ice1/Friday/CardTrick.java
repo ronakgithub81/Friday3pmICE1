b4522e9be038376fb9e2d781c9f8f09943e38c0d
SeanCBrachvogel <47042313+SeanCBrachvogel@users.noreply.github.com>
2019-02-01 16:41:20 -0500

    Show suit
    Sean brachvogel

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,7 +8,8 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+         Card card = new Card();
+        System.out.println(card.getSuit());
     }
     
 }
