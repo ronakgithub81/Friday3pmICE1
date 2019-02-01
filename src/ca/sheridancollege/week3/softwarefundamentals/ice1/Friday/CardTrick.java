9ae74a6dbf10d98dde08ad2df4c9be5f67c2a2c7
chungsup <limchu@sheridancollege.ca>
2019-02-02 06:48:06 +0900

    adding integer j variable

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,12 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Chungsup Lim 991515450
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card limCard = new Card();
     }
     
 }
