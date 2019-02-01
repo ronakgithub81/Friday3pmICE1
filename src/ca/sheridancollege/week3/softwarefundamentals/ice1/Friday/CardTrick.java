53e08653684b7f79783ae96370ccff1e9a0cce44
Javier <nievasj@sheridancollege.ca>
2019-02-01 17:07:15 -0500

    Commit after create 2 Objects and print it

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,6 +9,13 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card card1 = new Card();
+       Card card2 = new Card();
+       
+       
+        System.out.printf("Suit is: %s, and Value is: %s" ,card1.getSuit(), card1.getValue());
+        System.out.printf("Suit is: %s, and Value is: %s" ,card2.getSuit(), card2.getValue());       
     }
+    
+    
     
 }
