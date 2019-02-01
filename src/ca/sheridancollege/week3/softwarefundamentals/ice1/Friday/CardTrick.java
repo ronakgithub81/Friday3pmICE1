d2fc723dbd6b01bf9fb724faf53467513d9ca6df
nodasgithub <nodas@sheridan.desire2learn.com>
2019-02-01 16:52:02 -0500

    commiting ICE 1 version of my StudentDemo code

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -1,14 +1,15 @@
-
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
  * @author Ronak
+ * Name: Shinnosuke Noda
+ * Student ID: 991527227
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card card1 = new Card();
     }
     
 }
