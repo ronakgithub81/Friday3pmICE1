52639f14f0468e892e688fd193f196786797960b
Mohammad Shoeb <35189663+md-shoeb@users.noreply.github.com>
2019-02-01 16:50:26 -0500

    Created and printed results for suit, and value
    Created and printed results for suit, and value

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -10,6 +10,12 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card card1 = new Card();
+        
+        card1.setSuit("Diamond");
+        cardq.setValue(5);
+        
+        System.out.println("Suit: "+ card1.getSuit());
+        System.out.println("Value: "+ card.getValue());
        
     }
     
