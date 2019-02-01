0b3262bcb5f007620a04ae474ecff05de10dd60f
telferja <telferja@sheridancollege.ca>
2019-02-01 17:03:42 -0500

    Created an object of card class

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -4,11 +4,12 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 /**
  *
  * @author Ronak
+ * James Telfer / 991507400
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card card = new Card();
     }
     
 }
