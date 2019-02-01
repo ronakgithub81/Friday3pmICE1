6d0317190893f4e9fd3e2c7193fbd0ff82c10cbd
Wennn <wenb@sheridancollege.ca>
2019-02-01 16:42:56 -0500

    adding an object of Card class in Cadtrick.java file

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -2,12 +2,15 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
- *
+ * Name: Wen Boqian
+ * Id: 991427201
  * @author Ronak
  */
 public class CardTrick {
     public static void main(String[] args)
     {
+        Card c1=new Card();
+        
        
     }
     
