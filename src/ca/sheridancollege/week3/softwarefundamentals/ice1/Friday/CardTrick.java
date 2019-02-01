f40aa6a460b8bbd5b6a28daaa60836577ef72d85
hamzsajjad150 <khanhamza150@outlook.com>
2019-02-01 16:48:34 -0500

    committing first version of the software_fundamental

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,6 +8,11 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
+       Card c1 = new Card();
+       
+       c1.setSuit("Hearts");
+       c1.setValue(12);
+       
        
     }
     
