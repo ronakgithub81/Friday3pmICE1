6ee248af228daed51588b16caad1f4ab158f22fe
yuanqimia <wuqin@shernet.sheridancollege.ca>
2019-02-01 16:50:16 -0500

    object created

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -4,11 +4,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 /**
  *
  * @author Ronak
+ * @modifier Qingqing Wu/ 991500423
+ * 
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card card = new Card();
     }
     
 }
