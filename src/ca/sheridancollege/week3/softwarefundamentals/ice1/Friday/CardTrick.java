6c39b4229c27a3b5cc7c738ea8ff62725f849df6
James Burrows <burrojam@sheridancollege.ca>
2019-02-01 16:48:18 -0500

    added int j and fixed spelling mistakes blem

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -4,11 +4,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 /**
  *
  * @author Ronak
+ * & James Burrows 991541595
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card c1;
+        c1 = new Card();
     }
     
 }
