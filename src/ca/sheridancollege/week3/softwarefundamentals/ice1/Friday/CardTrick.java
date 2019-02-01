bac339349cadcec35d9b5b0048fa5e25d4d7b06c
truemanj <truemanj@shernet.sheridancollege.ca>
2019-02-01 16:47:52 -0500

    Added a card object

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,12 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Josh Trueman 991257988
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card spades = new Card();
     }
     
 }
