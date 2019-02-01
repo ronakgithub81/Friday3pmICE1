d268fc6f6684fb1ea9fe2c858f88d1e95c1fcf09
burning-cinnamon <41842208+burning-cinnamon@users.noreply.github.com>
2019-02-01 16:56:18 -0500

    created new card object and added comments

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,15 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Karam Jawad
+ * @studentNum 991507691
  */
 public class CardTrick {
     public static void main(String[] args)
+    
     {
-       
+        
+       Card trick = new Card();
     }
     
 }
