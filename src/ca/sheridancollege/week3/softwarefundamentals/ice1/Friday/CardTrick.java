b01b2075dc7fcec83e9d700bb2fbd7c1464b0ea4
chungsup <limchu@sheridancollege.ca>
2019-02-02 07:19:59 +0900

    adding integer j variable

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,11 +8,12 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-       Card limCard = new Card();
+        Card limCard = new Card();
         limCard.setValue(11);
         limCard.setSuit(Spades);
         System.out.println(limCard.getValue());
         System.out.println(limCard.getSuit());
+        Card lim2Card = new Card();
     }
     
 }
