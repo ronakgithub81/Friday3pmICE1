9f7d8058f04147b3ffeb82990324e8e4b1a4a5a7
Berat Sen <senbe@sheridancollege.ca>
2019-02-01 16:54:12 -0500

    added second object and fixed print statements

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -14,8 +14,13 @@ public class CardTrick {
         cardObject.setSuit("Hearts");
         cardObject.setValue(5);
         
-        System.out.println(cardObject.getSuit() + "" + cardObject.getValue());
+        System.out.println(cardObject.getValue() + " of " + cardObject.getSuit());
         
+        Card card2 = new Card();
+        card2.setSuit("Diamonds");
+        card2.setValue(10);
+        
+        System.out.println(card2.getValue() + " of " + card2.getSuit() );
     }
     
 }
